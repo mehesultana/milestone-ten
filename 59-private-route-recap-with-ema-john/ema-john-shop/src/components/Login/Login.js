5728c92ab9bf0782cc99f35435eff6d74e3e641f
mehesultana <sultanamehe01@gmail.com>
2021-10-10 00:04:25 +0600

    Create login, register component and set route

---
+++ 59-private-route-recap-with-ema-john/ema-john-shop/src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+	return (
+		<div className="login-form">
+			<div>
+				<h2>Login</h2>
+				<form onSubmit=""></form>
+				<input type="email" name="" id="" placeholder="Your Email" />
+				<br />
+				<input type="password" name="" id="" />
+				<br />
+				<input type="submit" />
+				<p>
+					New to ema-John <Link to="/register">Create Account</Link>
+				</p>
+				<div>------or------</div>
+				<button className="btn-regular">Google Sign In</button>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
