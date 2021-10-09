9c7b101bd52c2903420bf0b714aa4fc562aae4a0
mehesultana <sultanamehe01@gmail.com>
2021-10-10 01:58:23 +0600

    Use Auth Context hook

--- 59-private-route-recap-with-ema-john/ema-john-shop/src/components/Header/Header.js
+++ 59-private-route-recap-with-ema-john/ema-john-shop/src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import useAuth from '../../hooks/useAuth';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
-	const { user, logOut } = useFirebase();
+	const { user, logOut } = useAuth();
 
 	return (
 		<div className="header">
@@ -14,6 +14,9 @@ const Header = () => {
 				<NavLink to="/shop">Shop</NavLink>
 				<NavLink to="/review">Order Review</NavLink>
 				<NavLink to="/inventory">Manage Inventory</NavLink>
+
+				{user.email && <span style={{ color: 'white' }}>Hello {user.displayName}</span>}
+
 				{user.email ? <button onClick={logOut}>Log out</button> : <NavLink to="/login">Login</NavLink>}
 			</nav>
 		</div>
