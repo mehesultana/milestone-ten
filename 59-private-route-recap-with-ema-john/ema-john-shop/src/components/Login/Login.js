9e5aa175485a6eac6c803aa556a3ff18de671279
mehesultana <sultanamehe01@gmail.com>
2021-10-10 18:29:54 +0600

    Set Private Route and Redirect to login url

--- 59-private-route-recap-with-ema-john/ema-john-shop/src/components/Login/Login.js
+++ 59-private-route-recap-with-ema-john/ema-john-shop/src/components/Login/Login.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import './Login.css';
 
 const Login = () => {
 	const { signInUsingGoogle } = useAuth();
+	const location = useLocation();
+	console.log('came from', location.state?.from);
 
 	return (
 		<div className="login-form">
 			<div>
 				<h2>Login</h2>
-				<form onSubmit=""></form>
-				<input type="email" name="" id="" placeholder="Your Email" />
-				<br />
-				<input type="password" name="" id="" />
-				<br />
-				<input type="submit" />
-				<p>
-					New to ema-John <Link to="/register">Create Account</Link>
-				</p>
-				<div>------or------</div>
-				<button className="btn-regular" onClick={signInUsingGoogle}>
-					Google Sign In
-				</button>
+				<form>
+					<input type="email" name="" id="" placeholder="Your Email" />
+					<br />
+					<input type="password" name="" id="" />
+					<br />
+					<input type="submit" />
+					<p>
+						New to ema-John <Link to="/register">Create Account</Link>
+					</p>
+					<div>------or------</div>
+					<button className="btn-regular" onClick={signInUsingGoogle}>
+						Google Sign In
+					</button>
+				</form>
 			</div>
 		</div>
 	);
--- 59-private-route-recap-with-ema-john/ema-john-shop/src/components/PrivateRoute/PrivateRoute.js
+++ 59-private-route-recap-with-ema-john/ema-john-shop/src/components/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import useAuth from '../../hooks/useAuth';
+import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ children, ...rest }) => {
-	return <div></div>;
+	const { user } = useAuth();
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				user.email ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: '/login',
+							state: { from: location },
+						}}
+					></Redirect>
+				)
+			}
+		></Route>
+	);
 };
 
 export default PrivateRoute;
