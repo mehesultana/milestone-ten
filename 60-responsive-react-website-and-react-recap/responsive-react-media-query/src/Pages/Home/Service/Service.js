d1b450b336e6f589b684553a5061934ef3e405b4
mehesultana <sultanamehe01@gmail.com>
2021-10-12 02:36:31 +0600

    Make Services Responsive using CSS media query

--- 60-responsive-react-website-and-react-recap/responsive-react-media-query/src/Pages/Home/Service/Service.js
+++ 60-responsive-react-website-and-react-recap/responsive-react-media-query/src/Pages/Home/Service/Service.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({ service }) => {
 	// const {service} = props ;
 	const { name, price, description, img } = service;
 
 	return (
-		<div>
+		<div className="service">
 			<img src={img} alt="" />
 			<h3>{name}</h3>
 			<h5>Price : {price}</h5>
