ca7475da99a70f3470867dc6826c0b171e0ea1bd
mehesultana <sultanamehe01@gmail.com>
2021-10-13 01:06:16 +0600

    Header and Login component with React Bootstrap Navbar

--- 61-react-router-and-firebase-auth-recap/genius-car-mechanics/src/App.js
+++ 61-react-router-and-firebase-auth-recap/genius-car-mechanics/src/App.js
@@ -3,17 +3,23 @@ import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Booking from './Pages/Booking/Booking/Booking';
+import Login from './Pages/Login/Login/Login';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
+				<Header></Header>
 				<Switch>
 					<Route exact path="/">
 						<Home></Home>
 					</Route>
 					<Route path="/home">
 						<Home></Home>
+					</Route>
+					<Route path="/login">
+						<Login></Login>
 					</Route>
 					<Route path="/booking/:serviceId">
 						<Booking></Booking>
