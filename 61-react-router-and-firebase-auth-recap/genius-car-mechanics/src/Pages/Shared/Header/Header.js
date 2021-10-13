6654fe87c823c10a56d908b1c8c7223fc340bfec
mehesultana <sultanamehe01@gmail.com>
2021-10-13 21:37:05 +0600

    Fix Reload redirect to login page issue and Firebase deploy

--- 61-react-router-and-firebase-auth-recap/genius-car-mechanics/src/Pages/Shared/Header/Header.js
+++ 61-react-router-and-firebase-auth-recap/genius-car-mechanics/src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../../hooks/useAuth';
 
 const Header = () => {
@@ -13,13 +14,13 @@ const Header = () => {
 					<Navbar.Brand href="#home">Genius Mechanics</Navbar.Brand>
 					<Navbar.Toggle />
 					<Navbar.Collapse className="justify-content-end">
-						<Nav.Link as={Link} to="/home#home">
+						<Nav.Link as={HashLink} to="/home#home">
 							Home
 						</Nav.Link>
-						<Nav.Link as={Link} to="/home#services">
+						<Nav.Link as={HashLink} to="/home#services">
 							Services
 						</Nav.Link>
-						<Nav.Link as={Link} to="/home#experts">
+						<Nav.Link as={HashLink} to="/home#experts">
 							Experts
 						</Nav.Link>
 
